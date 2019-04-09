e7ec5a339ec2bcd030d833a146342e9ec126b262
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-04-09 13:55:13 +0200

    Add initial DDL

---
+++ src/main/java/edu/sdu/sensumbosted/data/ddl.sql
@@ -0,0 +1,32 @@
+-- auto-generated definition
+create table department
+(
+    id   uuid not null
+        constraint department_pk
+            primary key,
+    name text not null
+);
+
+alter table department
+    owner to kjpusdpl;
+
+create unique index department_id_uindex
+    on department (id);
+
+-- auto-generated definition
+create table "user"
+(
+    id         uuid not null
+        constraint user_pk
+            primary key,
+    department uuid,
+    name       text,
+    auth       smallint
+);
+
+alter table "user"
+    owner to kjpusdpl;
+
+create unique index user_id_uindex
+    on "user" (id);
+
