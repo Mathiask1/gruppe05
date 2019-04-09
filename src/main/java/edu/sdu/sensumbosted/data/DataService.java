b139081f7de419eeb2a4ba2abaeccc59e76c049f
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-04-09 11:03:58 +0200

    Configure db connections

--- src/main/java/edu/sdu/sensumbosted/data/DataService.java
+++ src/main/java/edu/sdu/sensumbosted/data/DataService.java
@@ -1,18 +1,23 @@
 package edu.sdu.sensumbosted.data;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import edu.sdu.sensumbosted.AuditAction;
 import edu.sdu.sensumbosted.entity.Context;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 public class DataService {
 
-    /*private final javax.sql.DataSource dataSource;
+    private final DataSource dataSource;
     private final JdbcTemplate departments;
 
     public DataService()  {
-        dataSource = new PGSimpleDataSource();
-        // TODO
+        HikariConfig conf = new HikariConfig("database.properties");
+        dataSource = new HikariDataSource(conf);
         departments = new JdbcTemplate(dataSource);
-    }*/
+    }
 
     public void log(Context ctx, AuditAction action) {
         log(ctx, action, "");
