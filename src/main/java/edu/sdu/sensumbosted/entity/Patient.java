c3ad54c4b18c236620230441e222d65de94f335d
Mathiask1 <birkebjerg99@live.dk>
2019-04-09 11:20:38 +0200

    Update Patient.java

--- src/main/java/edu/sdu/sensumbosted/entity/Patient.java
+++ src/main/java/edu/sdu/sensumbosted/entity/Patient.java
@@ -3,12 +3,13 @@ import edu.sdu.sensumbosted.AuditAction;
 import edu.sdu.sensumbosted.Util;
 
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
 public class Patient extends User {
 
-    private Map<Integer, String> diary;
+    private Map<Long, String> diary;
     private List<String> asignees;
     private boolean enrolled;
     private String id;
@@ -23,7 +24,7 @@ public class Patient extends User {
         return AuthLevel.PATIENT;
     }
 
-    public Map<Integer, String> getDiary(Context ctx) {
+    public Map<Long, String> getDiary(Context ctx) {
         ctx.assertAndLog(AuditAction.DIARY_READ, AuthLevel.PRACTITIONER);
         return this.diary;
     }
