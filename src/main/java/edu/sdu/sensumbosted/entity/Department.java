f291fd842333b05025bdbbc93c9aac4f3da70b7a
Mathiask1 <birkebjerg99@live.dk>
2019-04-09 11:06:52 +0200

    Update Department.java

--- src/main/java/edu/sdu/sensumbosted/entity/Department.java
+++ src/main/java/edu/sdu/sensumbosted/entity/Department.java
@@ -16,45 +16,45 @@ public class Department {
     }
 
     public void newManager(Context ctx, String name, AuthLevel auth) {
-        ctx.assertAndLog(AuditAction.USER_CREATE,AuthLevel.LOCAL_ADMIN);
+        ctx.assertAndLog(AuditAction.USER_CREATE, AuthLevel.LOCAL_ADMIN);
         User manager = new Manager(name, auth);
         members.put(manager.getId(),manager);
     }
 
     public void newPractitioner(Context ctx, String name) {
-        ctx.assertAndLog(AuditAction.USER_CREATE,AuthLevel.LOCAL_ADMIN);
+        ctx.assertAndLog(AuditAction.USER_CREATE, AuthLevel.LOCAL_ADMIN);
         User practitioner = new Practitioner(name);
         members.put(practitioner.getId(),practitioner);
     }
 
     public void newPatient(Context ctx, String name) {
-        ctx.assertAndLog(AuditAction.USER_CREATE,AuthLevel.CASEWORKER);
+        ctx.assertAndLog(AuditAction.USER_CREATE, AuthLevel.CASEWORKER);
         User patient = new Patient(name);
-        members.put(patient.getId(),patient);
+        members.put(patient.getId(), patient);
     }
 
     public void deleteUser(Context ctx, User user) {
-        ctx.assertAndLog(AuditAction.USER_DELETE,AuthLevel.LOCAL_ADMIN);
+        ctx.assertAndLog(AuditAction.USER_DELETE, AuthLevel.LOCAL_ADMIN);
         members.remove(user);
     }
 
     public User getUser(Context ctx, String id) {
-        ctx.assertAndLog(AuditAction.PRACTITIONER_READ_ASSIGNED,AuthLevel.PRACTITIONER);
+        ctx.assertAndLog(AuditAction.PRACTITIONER_READ_ASSIGNED, AuthLevel.PRACTITIONER);
         return members.get(id);
     }
 
     public Manager getManager(Context ctx, String id) {
-        ctx.assertAndLog(AuditAction.MANAGER_READ,AuthLevel.PATIENT);
+        ctx.assertAndLog(AuditAction.MANAGER_READ, AuthLevel.PATIENT);
         return (Manager) members.get(id);
     }
 
     public Practitioner getPractitioner(Context ctx, String id) {
-        ctx.assertAndLog(AuditAction.PRACTITIONER_READ,AuthLevel.PATIENT);
+        ctx.assertAndLog(AuditAction.PRACTITIONER_READ, AuthLevel.PATIENT);
         return (Practitioner) members.get(id);
     }
 
     public Patient getPatient(Context ctx, String id) {
-        ctx.assertAndLog(AuditAction.PATIENT_READ,AuthLevel.PATIENT);
+        ctx.assertAndLog(AuditAction.PATIENT_READ, AuthLevel.PATIENT);
         return (Patient) members.get(id);
     }
 
