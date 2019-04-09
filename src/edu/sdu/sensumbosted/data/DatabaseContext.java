8ff3ae86cc4f0303e0d49641aa1bd0730ca21527
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-04-09 10:30:31 +0200

    Override methods in DatabaseContext

--- src/edu/sdu/sensumbosted/data/DatabaseContext.java
+++ src/edu/sdu/sensumbosted/data/DatabaseContext.java
@@ -1,7 +1,9 @@
 package edu.sdu.sensumbosted.data;
 
+import edu.sdu.sensumbosted.AuditAction;
 import edu.sdu.sensumbosted.entity.AuthLevel;
 import edu.sdu.sensumbosted.entity.Context;
+import edu.sdu.sensumbosted.entity.User;
 
 /**
  * This is used by the data layer for the purpose of bypassing the permission system
@@ -14,5 +16,29 @@ public final class DatabaseContext extends Context {
     @Override
     public AuthLevel getAuth() {
         return AuthLevel.SUPERUSER;
+    }
+
+    @Override
+    public void assertAndLog(AuditAction action, AuthLevel auth) {}
+
+    @Override
+    public boolean checkMinimum(AuthLevel auth) { return true; }
+
+    @Override
+    public void assertAndLog(AuditAction action, AuthLevel auth, Runnable runnable) {
+        runnable.run();
+    }
+
+    @Override
+    public void assertMinimum(AuditAction action, AuthLevel auth) { }
+
+    @Override
+    public void setUser(User user) {
+        throw new UnsupportedOperationException("Attempt to set the user in the database context");
+    }
+
+    @Override
+    public User getUser() {
+        return null;
     }
 }
