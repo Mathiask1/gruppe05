14536bd7b93a1d506a477ef10449750bef6d80b6
Mathiask1 <birkebjerg99@live.dk>
2019-04-12 08:14:47 +0200

    test for #git on discord

---
+++ src/main/java/edu/sdu/sensumbosted/presentation/MainGUI.java
@@ -0,0 +1,19 @@
+package edu.sdu.sensumbosted.presentation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sensum Bosted");
+
+        primaryStage.show();
+
+    }
+}
---
+++ src/main/java/edu/sdu/sensumbosted/presentation/controllers/Login.java
@@ -0,0 +1,8 @@
+package edu.sdu.sensumbosted.presentation.controllers;
+
+import javafx.scene.input.MouseEvent;
+
+public class Login {
+    public void mouseclicked(MouseEvent mouseEvent) {
+    }
+}
