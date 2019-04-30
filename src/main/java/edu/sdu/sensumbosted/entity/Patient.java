510c7bb77bf9bd7ced43706d924313a7e1115498
Mathiask1 <birkebjerg99@live.dk>
2019-04-30 11:19:11 +0200

    Added newDiaryEntry og ændret setDiary til editDiary

--- src/main/java/edu/sdu/sensumbosted/entity/Patient.java
+++ src/main/java/edu/sdu/sensumbosted/entity/Patient.java
@@ -4,6 +4,7 @@ import edu.sdu.sensumbosted.Util;
 
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -30,9 +31,15 @@ public class Patient extends User {
         return this.diary;
     }
 
-    public void setDiary(Context ctx, String diaryEntry) {
+    public void editDiary(Context ctx, long diaryId, String diaryEntry) {
         ctx.assertAndLog(AuditAction.DIARY_WRITE, AuthLevel.PRACTITIONER);
-        long diaryId = TimeUnit.DAYS.toMillis(1);
+        diary.put(diaryId, diaryEntry);
+    }
+
+    public void newDiaryEntry(Context ctx, String diaryEntry) {
+        ctx.assertAndLog(AuditAction.DIARY_WRITE, AuthLevel.PRACTITIONER);
+        LocalDate localDate = LocalDate.now();
+        long diaryId = TimeUnit.DAYS.toMillis(localDate.toEpochDay());
         diary.put(diaryId, diaryEntry);
     }
 }
