e6abcc26c9e37407cb3cc8dc0ab69a4be7283926
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-05-13 00:58:43 +0200

    Set stage options

--- src/main/java/edu/sdu/sensumbosted/Main.java
+++ src/main/java/edu/sdu/sensumbosted/Main.java
@@ -35,7 +35,9 @@ public class Main extends Application {
 
         Scene scene = new Scene(root);
 
-        stage.setTitle("FXML Welcome");
+        stage.setMinWidth(640);
+        stage.setMinHeight(480);
+        stage.setTitle("Sensum Bosted");
         stage.setScene(scene);
         stage.show();
     }
