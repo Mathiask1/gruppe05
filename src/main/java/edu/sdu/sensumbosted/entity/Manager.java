a89fa5f02f8d228c493375c4338bbb07110bb026
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-05-12 16:58:30 +0200

    Add #setAuthLevel()

--- src/main/java/edu/sdu/sensumbosted/entity/Manager.java
+++ src/main/java/edu/sdu/sensumbosted/entity/Manager.java
@@ -1,5 +1,7 @@
 package edu.sdu.sensumbosted.entity;
 
+import edu.sdu.sensumbosted.AuditAction;
+
 public class Manager extends User {
 
     public static final String SQL_TABLE = "managers";
@@ -18,6 +20,24 @@ public class Manager extends User {
     @Override
     public AuthLevel getAuth() {
         return authLevel;
+    }
+
+    public void setAuthLevel(Context ctx, AuthLevel newLevel) {
+        if (newLevel == AuthLevel.PATIENT || newLevel == AuthLevel.PRACTITIONER)
+            throw new IllegalArgumentException(newLevel + " should not be managed by this class");
+
+        if (ctx.getAuth().ordinal() < authLevel.ordinal())
+            throw new SensumAccessException(ctx, AuditAction.MANAGER_AUTH_CHANGE,
+                    "Cannot change auth level of more privileged user.");
+
+        if (!ctx.checkMinimum(newLevel)) throw new SensumAccessException(ctx, AuditAction.MANAGER_AUTH_CHANGE,
+                "Cannot change to auth level higher than your level.");
+
+        // Admin and up and change perms
+        ctx.assertAndLog(getDepartment(), AuditAction.MANAGER_AUTH_CHANGE, AuthLevel.LOCAL_ADMIN, () -> {
+            authLevel = newLevel;
+            ctx.data.update(this);
+        });
     }
 
     @Override
