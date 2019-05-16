fe5354a7fdef491e5ecc729ec0361e8c270161e8
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-05-16 22:43:30 +0200

    Refactor how Manager deals with auth assignment

--- src/main/java/edu/sdu/sensumbosted/entity/Manager.java
+++ src/main/java/edu/sdu/sensumbosted/entity/Manager.java
@@ -3,6 +3,7 @@ package edu.sdu.sensumbosted.entity;
 import edu.sdu.sensumbosted.AuditAction;
 import edu.sdu.sensumbosted.Util;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class Manager extends User {
@@ -25,22 +26,33 @@ public class Manager extends User {
         return authLevel;
     }
 
-    public void setAuthLevel(Context ctx, AuthLevel newLevel) {
+    /**
+     * Check if we can assign the auth level
+     * @return An empty Optional if we are allowed, or a reason why we can't.
+     */
+    public Optional<String> canSetAuthLevel(Context ctx, AuthLevel newLevel) {
         if (newLevel == AuthLevel.PATIENT || newLevel == AuthLevel.PRACTITIONER)
-            throw new IllegalArgumentException(newLevel + " should not be managed by this class");
+            return Optional.of(newLevel + " should not be managed by this class");
 
         if (ctx.getAuth().ordinal() < authLevel.ordinal())
-            throw new SensumAccessException(ctx, AuditAction.MANAGER_AUTH_CHANGE,
-                    "Cannot change auth level of more privileged user.");
+            return Optional.of("Cannot change auth level of more privileged user.");
 
-        if (!ctx.checkMinimum(newLevel)) throw new SensumAccessException(ctx, AuditAction.MANAGER_AUTH_CHANGE,
-                "Cannot change to auth level higher than your level.");
+        if (!ctx.checkMinimum(newLevel)) return Optional.of("Cannot change to auth level higher than your level.");
 
         // Admin and up and change perms
-        ctx.assertAndLog(getDepartment(), AuditAction.MANAGER_AUTH_CHANGE, AuthLevel.LOCAL_ADMIN, () -> {
-            authLevel = newLevel;
-            ctx.data.update(this);
+        if (!ctx.checkMinimum(getDepartment(), AuthLevel.LOCAL_ADMIN)) return Optional.of("Must be LOCAL_ADMIN or above");
+
+        return Optional.empty();
+    }
+
+    public void setAuthLevel(Context ctx, AuthLevel newLevel) {
+        canSetAuthLevel(ctx, newLevel).ifPresent(s -> {
+            throw new SensumAccessException(ctx, AuditAction.MANAGER_AUTH_CHANGE, s);
         });
+
+        ctx.data.log(ctx, AuditAction.MANAGER_AUTH_CHANGE, "Changing auth of " + this + " to " + newLevel);
+        authLevel = newLevel;
+        ctx.data.update(this);
     }
 
     @Override
